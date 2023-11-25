351dbfcf2b5579dd193c747d62aca0d2ec6baa65
Simon Manigel <simon@manigel.eu>
2023-11-25 04:08:30 +0100

    Logger Klasse hinzufügen.

---
+++ ExternDLL/Logger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace ExternDLL
+{
+    public class Logger
+    {
+        private string logDirectory;
+
+        public Logger(string logDirectory)
+        {
+            this.logDirectory = logDirectory;
+
+            // Stellen Sie sicher, dass das Protokollverzeichnis vorhanden ist
+            if (!Directory.Exists(logDirectory))
+            {
+                Directory.CreateDirectory(logDirectory);
+            }
+        }
+
+        public void LogRequest(string method, string parameters)
+        {
+            string logFileName = GetLogFileName();
+
+            string logMessage = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff} - Method: {method}, Parameters: {parameters}";
+
+            File.AppendAllText(logFileName, logMessage + Environment.NewLine);
+        }
+
+
+        private string GetLogFileName()
+        {
+            string currentDate = DateTime.Now.ToString("yyyy-MM-dd");
+            string logFileName = Path.Combine(logDirectory, $"log_{currentDate}.txt");
+            return logFileName;
+        }
+    }
+}
