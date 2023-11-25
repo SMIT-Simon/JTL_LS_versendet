4d7a73b2e4e96b18c769075cf9a4f4e4986227fc
Simon Manigel <simon@manigel.eu>
2023-11-25 04:08:30 +0100

    Projektdateien hinzufügen.

---
+++ ExternDLL/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ExternDLL;
+
+namespace ExternDLL
+{
+    static class Program
+    {
+        static HttpListener listener;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            StartServer();
+        }
+
+        static void StartServer()
+        {
+            // Initialisieren des HttpListener, der HTTP-Anfragen entgegennimmt
+            listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:5000/"); // Definieren des Endpunkts (URL)
+            listener.Start();
+            Console.WriteLine("Listening for connections on http://localhost:5000/");
+
+            // Endlosschleife für das kontinuierliche Hören auf Anfragen
+            while (true)
+            {
+                HandleIncomingConnections().GetAwaiter().GetResult();
+            }
+        }
+
+        static async Task HandleIncomingConnections()
+        {
+            int kUser = 0; // Vorab initialisieren
+            int kLieferschein = 0; // Vorab initialisieren
+
+            // Warten auf eine eingehende HTTP-Anfrage
+            HttpListenerContext ctx = await listener.GetContextAsync();
+
+            HttpListenerRequest req = ctx.Request;
+            HttpListenerResponse resp = ctx.Response;
+
+            if (req.HttpMethod == "POST")
+            {
+                using (Stream body = req.InputStream)
+                using (StreamReader reader = new StreamReader(body, req.ContentEncoding))
+                {
+                    // Lesen des POST-Datenstroms (Parameter der Anfrage)
+                    string postData = reader.ReadToEnd();
+                    string[] parameters = postData.Split(new[] { '&' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    // Verarbeiten der empfangenen Parameter
+                    foreach (string parameter in parameters)
+                    {
+                        string[] keyValue = parameter.Split('=');
+                        if (keyValue.Length == 2)
+                        {
+                            string key = keyValue[0];
+                            string value = keyValue[1];
+
+                            if (key == "kUser")
+                            {
+                                int.TryParse(value, out kUser);
+                            }
+                            else if (key == "kLieferschein")
+                            {
+                                int.TryParse(value, out kLieferschein);
+                            }
+                        }
+                    }
+                }
+
+                // Hier werden die Parameter an JTL_WorkflowLieferschein übergeben
+                Worker worker = new Worker();
+                string server = "localhost\\JTLWAWI";
+                string datenbank = "eazybusiness";
+                string benutzer = "sa";
+                string passwort = "sa04jT14";
+                int eventID = 3; // Event-ID für "Lieferschein versendet"
+
+                worker.JTL_WorkflowLieferschein(server, datenbank, benutzer, passwort, kUser, kLieferschein, eventID);
+
+                // Validieren der Assembly (falls notwendig)
+                ValidateAssembly validateAssembly = new ValidateAssembly();
+                if (!validateAssembly.IsValid)
+                    return;
+            }
+
+            // Erstellen der HTTP-Antwort
+            string responseString = $"KUser: {kUser}, KLieferschein: {kLieferschein}";
+            byte[] responseData = Encoding.UTF8.GetBytes(responseString);
+            resp.ContentType = "text/plain";
+            resp.ContentEncoding = Encoding.UTF8;
+            resp.ContentLength64 = responseData.LongLength;
+
+            // Senden der HTTP-Antwort
+            await resp.OutputStream.WriteAsync(responseData, 0, responseData.Length);
+            resp.Close();
+        }
+    }
+}
---
+++ ExternDLL/Worker.cs
@@ -0,0 +1,81 @@
+﻿using JTLwawiExtern;
+using System;
+using System.Windows.Forms;
+
+namespace ExternDLL
+{
+    public class Worker
+    {
+        private readonly CJTLwawiExtern _wawiExtern;
+
+        public Worker()
+        {
+            this._wawiExtern = new CJTLwawiExtern();
+        }
+
+        public string GetXmlFilePath()
+        {
+            string file = "";
+            using (var openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                openFileDialog.Title = @"XML auswählen";
+                openFileDialog.DefaultExt = ".xml";
+                openFileDialog.Filter = @"XML-Datei|*.xml";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    file = openFileDialog.FileName;
+                }
+            }
+            return file;
+        }
+
+        private bool HasConnection(string server, string datenbank, string benutzer, string passwort)
+        {
+            string fehler = "";
+            if (this._wawiExtern.JTL_TesteDatenbankverbindung(server, datenbank, benutzer, passwort, out fehler) == 1)
+                return true;
+            else
+            {
+                MessageBox.Show(string.Format("Fehler beim Verbinden {0}", fehler));
+                return false;
+            }
+        }
+
+        public void Import(string server, string datenbank, string benutzer, string passwort, int kBenutzer, string filePath)
+        {
+            if (HasConnection(server, datenbank, benutzer, passwort) && filePath.Length != 0)
+            {
+                string cKRechnungen;
+                this._wawiExtern.JTL_OrderXmlImport(server, datenbank, benutzer, passwort, kBenutzer, filePath, out cKRechnungen);
+                MessageBox.Show(cKRechnungen);
+            }
+        }
+
+        public void WorkFlow(string server, string datenbank, string benutzer, string passwort, int kBenutzer, int key, int id)
+        {
+            if (HasConnection(server, datenbank, benutzer, passwort))
+            {
+                this._wawiExtern.JTL_WorkflowAuftrag(server, datenbank, benutzer, passwort, kBenutzer, key, id);
+            }
+        }
+
+        public void JTL_WorkflowLieferschein(string server, string datenbank, string benutzer, string passwort, int kUser, int kLieferschein, int eventID)
+        {
+            if (HasConnection(server, datenbank, benutzer, passwort))
+            {
+                this._wawiExtern.JTL_WorkflowLieferschein(server, datenbank, benutzer, passwort, kUser, kLieferschein, eventID);
+            }
+        }
+
+
+
+        public void TestConnection(string server, string datenbank, string benutzer, string passwort)
+        {
+            if (HasConnection(server, datenbank, benutzer, passwort))
+            {
+                MessageBox.Show("Verbindung erfolgreich");
+            }
+        }
+    }
+}
